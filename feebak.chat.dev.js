1cb62de264567939e59ddba467cc8e34e63a6f95
Arjun Das T P <98824551+arjundastp@users.noreply.github.com>
2025-04-23 00:43:28 +0530

    Add files via upload
    init

---
+++ feebak.chat.dev.js
@@ -0,0 +1,31 @@
+let conversationId = '';
+var surveyLink = '';
+function feebakSaveConversation(e) {
+    conversationId = e.data.data.conversationId;
+}
+
+
+// function to be called upon chat end
+async function feebakOfferSurvey(data) {
+    // Call API to get the link
+    data.ConversationID = conversationId;
+
+    // change the link to app.feebak.com and remove the comment when going to prod
+    return await fetch('https://dev.feebak.com/v1/websurvey/link', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        if (!data.Success) {
+            console.error(data.Message);
+            return null;
+        } else {
+            surveyLink = data.Data;
+            return surveyLink;
+        }
+    });
+}
